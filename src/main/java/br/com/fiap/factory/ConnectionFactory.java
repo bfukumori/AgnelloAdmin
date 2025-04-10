99c67944d7d49969c3553138f9b658075bbb0b8a
Victor Jaber <victorjaber19@gmail.com>
2025-04-10 19:46:33 -0300

    adding cloud

--- src/main/java/br/com/fiap/factory/ConnectionFactory.java
+++ src/main/java/br/com/fiap/factory/ConnectionFactory.java
@@ -4,15 +4,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-//private static final String URL = "jdbc:mysql://145.223.95.225:3306/fiapdb";
-//private static final String USER = "fiapuser";
-//private static final String PWD = "userpass";
-
 public class ConnectionFactory {
-
-	private static final String URL = "jdbc:mysql://localhost:3306/meu_banco";
-	private static final String USER = "root";
-	private static final String PWD = "root";
+	
+	private static final String URL = "jdbc:mysql://145.223.95.225:3306/fiapdb";
+	private static final String USER = "fiapuser";
+	private static final String PWD = "userpass";
 
 	public static Connection getConnection() throws SQLException {
 		try {
